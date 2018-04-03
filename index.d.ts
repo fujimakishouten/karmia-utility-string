d1f4b627822d0500b7e049d6f9c0373ff2ec90c2
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-03 03:48:30 +0000

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,21 @@
-declare module KarmiaUtility {
-    export class KarmiaUtilityString {
-        isString(value: string): boolean;
-        strip(string: string, mask_characters: string): string;
-        lstrip(string: string, mask_characters: string): string;
-        rstrip(string: string, mask_characters: string): string;
-        trim(string: string, mask_characters: string): string;
-        ltrim(string: string, mask_characters: string): string;
-        rtrip(string: string, mask_characters: string): string;
-        normalize(string: string, form: string): string;
-        unquote(string: string): string;
-        zfill(number: string, width: number, encoding: string): string;
-        camelCase(words: string, capitalize: boolean): string;
-        snakeCase(words: string): string;
-        kebabCase(words: string): string;
-        parse(string: string, delimiter: RegExp|string, separator: RegExp|string, encoding: string): Object;
-        toBoolean(string: string): boolean;
-    }
+declare class KarmiaUtilityString {
+    constructor();
+    isString(value: any): boolean;
+    strip(string: string, mask_characters?: string): string;
+    lstrip(string: string, mask_characters?: string): string;
+    rstrip(string: string, mask_characters?: string): string;
+    trim(string: string, mask_characters?: string): string;
+    ltrim(string: string, mask_characters?: string): string;
+    rtrim(string: string, mask_characters?: string): string;
+    normalize(string: string, form?: string): string;
+    unquote(string: string): string;
+    zfill(number: string, width: number, encoding?: string): string;
+    camelCase(words: string, capitalize?: boolean): string;
+    snakeCase(words: string): string;
+    kebabCase(words: string): string;
+    parse(string: string, delimiter?: RegExp|string, separator?: RegExp|string, encoding?: string): object;
+    toBoolean(string: string): boolean;
 }
+
+declare function karmia_utility_string(options: object): KarmiaUtilityString;
+export = karmia_utility_string;
